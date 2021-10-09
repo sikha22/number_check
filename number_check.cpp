0f19c78bd75812210f3101ba390884f9e5efeb8d
sikha22 <84958102+sikha22@users.noreply.github.com>
2021-10-09 12:14:14 +0530

    Add files via upload
    Number check in c++

---
+++ number_check.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the value of variable you want to check as +ve or -ve:";
+    cin>>n;
+    if(n>0){
+        cout<<"Number is positive"<<endl;
+    }
+    else{
+        cout<<"Number is negative"<<endl;
+    }
+    return 0;
+}
